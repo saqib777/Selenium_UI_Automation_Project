69988fb86055522df7a1a1d063a2004a19e100f9
Mohammed Saqib <113831139+saqib777@users.noreply.github.com>
2026-01-04 22:42:29 +0530

    Update conftest.py

--- Code/conftest.py
+++ Code/conftest.py
@@ -1,1 +1,19 @@
+```
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
+
+@pytest.fixture
+def setup():
+    options = Options()
+    options.add_argument("--start-maximized")
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://parabank.parasoft.com/parabank/index.htm")
+
+    yield driver
+
+    driver.quit()
+```
